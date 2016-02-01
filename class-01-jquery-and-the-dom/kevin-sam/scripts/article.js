7e1faba37eed18ac175b822e62a780a8b2ec05df
Kevin Gifford <ksgifford@gmail.com>
2016-02-01 15:50:43 -0800

    Modified index.html and articles.js files to complete pair programming assignment.

---
+++ class-01-jquery-and-the-dom/kevin-sam/scripts/article.js
@@ -0,0 +1,51 @@
+var articles = [];
+
+function Article (opts) {
+  this.author = opts.author;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
+}
+
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+
+  $newArticle.attr('data-category', this.category);
+
+  $newArticle.find('header h1').text(this.title);
+
+  $newArticle.find('address a').text(this.author);
+
+  $newArticle.find('address a').attr('href', this.authorUrl);
+
+  $newArticle.find('.article-body').html(this.body);
+
+  $newArticle.find('time[datetime]').text(this.publishedOn);
+
+  // Includes the publication date as a 'title' attribute to show on hover:
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+
+  // Display the date as a relative number of "days ago":
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+
+  $newArticle.append('<hr>');
+
+  // TODO: This cloned article is no longer a template, so we should remove that class...
+  $newArticle.removeClass('template');
+
+  return $newArticle;
+}
+
+rawData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(function(ele) {
+  articles.push(new Article(ele));
+});
+
+articles.forEach(function(a){
+  $('#articles').append(a.toHtml())
+});
