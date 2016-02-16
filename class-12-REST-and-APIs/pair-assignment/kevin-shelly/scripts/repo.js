69e7d182994d29c9b9386d14e389528360231e6c
Kevin Gifford <ksgifford@gmail.com>
2016-02-16 12:27:23 -0800

    Initial commit for revisions based on Class 12 assignment.

---
+++ class-12-REST-and-APIs/pair-assignment/kevin-shelly/scripts/repo.js
@@ -0,0 +1,28 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  repos.requestRepos = function(callback) {
+    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    $.ajax({
+      url: 'https://api.github.com/users/ksgifford/repos' +
+            '?per_page=5&sort=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + gitHubToken},
+      success: function(data, message, xhr) {
+        console.log(data[0].description);
+      }
+    }).done(callback);
+  };
+
+  // DONE: Model method that filters the full collection for repos with a particular attribute.
+  // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
---
+++ class-12-REST-and-APIs/pair-assignment/kevin-shelly/scripts/routes.js
@@ -0,0 +1,7 @@
+// TODO-DONE: Configure routes for this app with page.js, by registering each URL your app can handle,
+// linked to a a single controller function to handle it:
+page('/', articlesController.index);
+page('/about', aboutController.index);
+
+// TODO-DONE: What function do you call to activate page.js? Fire it off now, to execute
+page();
